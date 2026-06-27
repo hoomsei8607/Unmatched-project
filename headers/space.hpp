633bab613cacf495ead617cf99fda5d763cc8b24
farzadNobahar <farzadpiczol22@gmail.com>
2026-06-27 11:45:57 -0700

    changed a memeber function name

--- headers/space.hpp
+++ headers/space.hpp
@@ -9,7 +9,7 @@ class Space
     void Change_Occupied_Status(); //reverses the occupied status
     bool Get_Occupied_Status();//returns wether  a space is occupied or not
     bool Get_Secret_Pathway_Status(); //returns a boolian wether a space is a secret path way or not
-    bool Is_Multi_Zone();
+    bool Get_Multi_Zone_status();
     void Get_Zone_Color(std::array<ZONE_COLORS,3>& Passed_Array_To_Be_Filled);
     void Set_Info(bool Is_Multi_zone, bool Is_Occupied, bool Has_Secret_Path, int Space_Number, const std::array<ZONE_COLORS, 3>& Colors);
 
