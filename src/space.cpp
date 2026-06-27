84855296bb30fc5d73aa8296e22c3c0d9270031c
farzadNobahar <farzadpiczol22@gmail.com>
2026-06-27 11:46:18 -0700

    wrote implementations for the memeber functions so far

---
+++ src/space.cpp
@@ -0,0 +1,42 @@
+#include "../headers/space.hpp"
+
+void Space::Change_Occupied_Status()
+{
+    this->Is_Occupied = !Is_Occupied;
+}
+
+bool Space::Get_Occupied_Status()
+{
+    return this->Is_Occupied;
+}
+
+bool Space::Get_Secret_Pathway_Status()
+{
+    return this->Has_Secret_Path;
+}
+
+bool Space::Get_Multi_Zone_status()
+{
+    return this->Is_Multi_Zone;
+}
+
+void Space::Get_Zone_Color(std::array <ZONE_COLORS, 3>& To_Be_Filled)
+{
+    for(int i = 0 ; i < 3 ; i++)
+    {
+        To_Be_Filled[i] = zone[i];
+    }
+}
+
+void Space::Set_Info(bool Is_Multi_zone, bool Is_Occupied, bool Has_Secret_Path, int Space_Number, const std::array<ZONE_COLORS, 3>& Colors)
+{
+    this->Is_Multi_Zone = Is_Multi_zone;
+    this->Is_Occupied = Is_Occupied;
+    this->Has_Secret_Path = Has_Secret_Path;
+    this->Space_Number = Space_Number;
+    for(int i = 0 ; i < 3 ; i++)
+    {
+        zone[i] = Colors[i];
+    }
+    
+}
