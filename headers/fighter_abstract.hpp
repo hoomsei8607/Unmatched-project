cc3c29517eb0f13525b4aaae1cebbfbcc72a0ab3
farzadNobahar <farzadpiczol22@gmail.com>
2026-06-27 14:07:28 -0700

    added a boolian attribute

--- headers/fighter_abstract.hpp
+++ headers/fighter_abstract.hpp
@@ -13,6 +13,7 @@ class Fighter_Base_Class
         int Base_Move_Value;
         int Current_Move_Value; //in case the player wants to boost the movement
         int Current_Hero_Space; //which space is hero standing on in the map
+        bool Is_Fighter_Alive;
         ATTACKING_RANGE Range_Type;
         FIGHTER_TYPE fighter_type;
 
