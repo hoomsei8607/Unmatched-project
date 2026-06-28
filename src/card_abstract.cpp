a372de094b655d5d53ec0c5cc3b89745d5b21970
Houman <hoomsei8607@gmail.com>
2026-06-28 12:16:49 +0330

    initialized the cards

---
+++ src/card_abstract.cpp
@@ -0,0 +1,30 @@
+#include "../headers/card_abstract.hpp"
+
+int Card_Base_Class::get_card_value()
+{
+    return Card_Value;
+}
+int Card_Base_Class::get_Card_Boost_Value()
+{
+    return Card_Boost_Value;
+}
+CARD_EFFECT_TYPE Card_Base_Class::get_effect()
+{
+    return effect;
+}
+CARD_TYPE Card_Base_Class::get_type()
+{
+    return type;
+}
+CARD_OWNER Card_Base_Class::get_owner()
+{
+    return owner;
+}
+bool Card_Base_Class::get_is_discarded()
+{
+    return is_discarded;
+}
+int Card_Base_Class::get_card_number()
+{
+    return card_number;
+}
