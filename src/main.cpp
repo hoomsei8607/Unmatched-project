001d2f630ed8ef26e237841096ac028c2533da6e
farzadNobahar <farzadpiczol22@gmail.com>
2026-06-28 12:59:40 -0700

    instantiated the ui object

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include "../headers/graph.hpp"
-
-
+#include "../headers/controller.hpp"
+#include "../headers/Ui.hpp"
 
 int main()
 {
+    User_Interface Interface_Controller;
+    while (true)
+    {
+        if(Interface_Controller.Manage_Screen())
+        {}
+        else
+        {
+            break;
+        }
+    }
     
+    return 0;
 }
