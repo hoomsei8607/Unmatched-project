6416588df9f15aa64e5684eefdc7211c8ad0061d
farzadNobahar <farzadpiczol22@gmail.com>
2026-06-28 12:57:57 -0700

    making the interface for tui

---
+++ headers/Ui.hpp
@@ -0,0 +1,21 @@
+#ifndef UI_HPP
+#define UI_HPP
+
+enum class SCREEN_STATE{MENU, GAME_LOOP, GAME_OVER, PROGRAM_SHOULD_TEMINATE, NONE};
+
+class User_Interface
+{
+    public:
+        User_Interface();
+        bool Manage_Screen();
+        void Starting_Menu_Screen();
+        // void Game_Loop_Screen();
+        // void Finished_Game_Screen();
+        
+    private:
+        SCREEN_STATE Current_Screen_State;
+
+};
+
+
+#endif
---
+++ src/Ui.cpp
@@ -0,0 +1,29 @@
+#include "../headers/Ui.hpp"
+
+
+User_Interface::User_Interface()
+{
+    this->Current_Screen_State = SCREEN_STATE::MENU;
+}
+
+bool User_Interface::Manage_Screen()
+{
+    switch (Current_Screen_State)
+    {
+    case SCREEN_STATE::MENU:
+        Starting_Menu_Screen();
+        return true;
+    
+    case SCREEN_STATE::GAME_LOOP:
+        // Game_Loop_Screen();
+        return true;
+    
+    case SCREEN_STATE::GAME_OVER:
+        // Finished_Game_Screen();
+        return true;
+
+    case SCREEN_STATE::PROGRAM_SHOULD_TEMINATE:
+        return false;
+    
+    }
+}
