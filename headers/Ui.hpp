6ed339254ca88437d7d331e3846391e68e3c6b46
farzadNobahar <farzadpiczol22@gmail.com>
2026-06-29 13:44:41 -0700

    added another enumeration to the enum and added the get user input member function

--- headers/Ui.hpp
+++ headers/Ui.hpp
@@ -1,7 +1,12 @@
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+
 #ifndef UI_HPP
 #define UI_HPP
 
-enum class SCREEN_STATE{MENU, GAME_LOOP, GAME_OVER, PROGRAM_SHOULD_TEMINATE, NONE};
+enum class SCREEN_STATE{MENU, GAME_LOOP, GAME_OVER, PROGRAM_SHOULD_TEMINATE, PROMPT_INPUT, NONE};
 
 class User_Interface
 {
@@ -10,6 +15,7 @@ class User_Interface
         bool Manage_Screen();
         void Starting_Menu_Screen();
         void Game_Loop_Screen();
+        void Get_User_Input();
         // void Finished_Game_Screen();
         
     private:
