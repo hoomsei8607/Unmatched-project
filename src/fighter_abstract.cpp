35976e9615bb91a9d78271009a9433d843c0b2e0
farzadNobahar <farzadpiczol22@gmail.com>
2026-06-29 13:42:54 -0700

    renamed the file

---
+++ src/fighter_abstract.cpp
@@ -0,0 +1,21 @@
+#include "../headers/fighter_abstract.hpp"
+
+int Fighter_Base_Class::Return_Fighter_Initial_Hp()
+{
+    return Initial_Hp;
+}
+
+int Fighter_Base_Class::Return_Fighter_Current_Hp()
+{
+    return Current_Hp;
+}
+
+int Fighter_Base_Class::Return_Fighter_Current_Move_Value()
+{
+    return Current_Move_Value;
+}
+
+int Fighter_Base_Class::Return_Fighter_Current_Space()
+{
+    return Current_Hero_Space;
+}
