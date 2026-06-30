0fe564abb84d214069dcf2f7eeff5a5394094bce
farzadNobahar <farzadpiczol22@gmail.com>
2026-06-30 10:43:02 -0700

    controller class file

--- headers/controller.hpp
+++ headers/controller.hpp
@@ -1,12 +1,15 @@
 #ifndef CONTROLLER_HPP
 #define CONTROLLER_HPP
-
+#include "user.hpp"
+#include "structs.hpp"
 class Controller
 {
     public:
+        void Set_User1_And_User2_Info(User1_And_User2_Info info_struct);
 
     private:
+        User user1;
+        User user2;
         
-
 };
 #endif
