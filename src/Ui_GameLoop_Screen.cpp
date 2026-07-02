bbebecf841b151d6a3b5605e3c6c3d4232c97cff
farzadNobahar <farzadpiczol22@gmail.com>
2026-07-02 11:44:38 -0700

    integrated placement screen manager class into the file.
    now it takes user input and places the sidekicks on the map
    we are ready to move on to the user action prompt screen

--- src/Ui_GameLoop_Screen.cpp
+++ src/Ui_GameLoop_Screen.cpp
@@ -1,13 +1,15 @@
 #include "../headers/Ui.hpp"
 #include "../headers/Ui_GameLoop_Screen_Related_Function.hpp"
 #include "../headers/controller.hpp"
+#include "../headers/main_game_loop_sideckick_placement_manager.hpp"
 #include <map>
 using namespace ftxui;
 
 
 void User_Interface::Game_Loop_Screen(Controller& control)
 {
-    
+    control.Set_Younger_User_Variable_Value();
+    SideKick_Placement_Screen_Manager side_kick_Screen_manager(control.Return_Younger_Hero_Name());
 
     User1_And_User2_Info user_info_struct;
     control.Fill_Users_Info_Struct(user_info_struct);
@@ -54,17 +56,33 @@ void User_Interface::Game_Loop_Screen(Controller& control)
     Map_Printing_Info[static_cast<int>(Fighters_Names::SIS1)].Is_Placed_On_Map = false;
     Map_Printing_Info[static_cast<int>(Fighters_Names::SIS2)].Is_Placed_On_Map = false;
     Map_Printing_Info[static_cast<int>(Fighters_Names::SIS3)].Is_Placed_On_Map = false;
+    Map_Printing_Info[static_cast<int>(Fighters_Names::SIS1)].Sis_Number = '1';
+    Map_Printing_Info[static_cast<int>(Fighters_Names::SIS2)].Sis_Number = '2';
+    Map_Printing_Info[static_cast<int>(Fighters_Names::SIS3)].Sis_Number = '3';
 
+    // auto to_be_looped_on = CatchEvent(render, [&](Event event){
+    //     if(event == Event::Character('q'))
+    //     {
+    //         screen.ExitLoopClosure()();
+    //         return true;
+    //     }
+    //     return false;
+        
+    // });
 
-    auto render = Renderer([&]{
+    auto Control_Prompt = vbox({
+            text("PRESS C") | bold ,
+            text("open control menu")
+        }) | border;
+
         control.Fill_Fighter_Info_Struct(Fighters_Names::DRACULA, Dracula_Info);
         control.Fill_Fighter_Info_Struct(Fighters_Names::SHERLOCK, Sherlock_Info);
         control.Fill_Fighter_Info_Struct(Fighters_Names::WATSON, Watson_Info);
         control.Fill_Fighter_Info_Struct(Fighters_Names::SIS1, Dracula_Sis1);
         control.Fill_Fighter_Info_Struct(Fighters_Names::SIS2, Dracula_Sis2);
         control.Fill_Fighter_Info_Struct(Fighters_Names::SIS3, Dracula_Sis3);
-
         Element Player1_HeroInfo_Box;
+
         if(user_info_struct.User1_Hero_Type == HERO_NAME::DRACULA)
         {
             Player1_HeroInfo_Box = vbox({
@@ -87,9 +105,8 @@ void User_Interface::Game_Loop_Screen(Controller& control)
                 
             }) | border;
         }
-        
         Element Player2_HeroInfo_Box;
-
+    
         if(user_info_struct.User2_Hero_Type == HERO_NAME::SHERLOCK)
         {
             Player2_HeroInfo_Box = vbox({
@@ -109,47 +126,34 @@ void User_Interface::Game_Loop_Screen(Controller& control)
                 hbox({text("Move Value: "), text(std::to_string(Dracula_Info.Move_Value))}),
                 hbox({text("Range: "), text(std::to_string(Dracula_Info.Range))}),
             }) | border;
-
+    
         }
-    
-        auto Control_Prompt = vbox({
-            text("PRESS C") | bold ,
-            text("open control menu")
-        }) | border;
-    
         auto Location_Info = vbox({
             text("Current Space Number: "), // fill the number
             text("Connected To: ") //fill the numbers
         }) | border;
+    while(true)
+    {
     
         auto Terminal_Out_Map = vbox
         ({
             paragraphAlignCenter(Return_Map_To_Be_Rendered(6, Map_Printing_Info))
         }) | border;
-    
-
-        return vbox({
+        
+        Element Users_Info_And_Map = vbox({
             hbox({
                 Player1_HeroInfo_Box,
                 Terminal_Out_Map,
                 Player2_HeroInfo_Box,
             }),
-            vbox({}) | border,
-            hbox({Control_Prompt, Location_Info})|border
         });
-    });
-
-    auto screen = ScreenInteractive::Fullscreen();
-    auto to_be_looped_on = CatchEvent(render, [&](Event event){
-        if(event == Event::Character('q'))
-        {
-            screen.ExitLoopClosure()();
-            return true;
-        }
-        return false;
         
-    });
-    screen.Loop(to_be_looped_on);
+        if(!side_kick_Screen_manager.Screen_Manager(control, Users_Info_And_Map, Map_Printing_Info))
+        {
+            break;
+        }
+    }
+
     Current_Screen_State = SCREEN_STATE::PROGRAM_SHOULD_TEMINATE;
-    
+    std::cout << Return_Map_To_Be_Rendered(6, Map_Printing_Info);
 }
