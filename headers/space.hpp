07beff427130ec373955ea015958ba681522d82f
farzadNobahar <farzadpiczol22@gmail.com>
2026-07-04 08:51:47 -0700

    added member a  function and implemented them
    i added const key word for the get functions and added a function to
    return the space number with

--- headers/space.hpp
+++ headers/space.hpp
@@ -7,10 +7,11 @@ class Space
 {
     public:
     void Change_Occupied_Status(); //reverses the occupied status
-    bool Get_Occupied_Status();//returns wether  a space is occupied or not
-    bool Get_Secret_Pathway_Status(); //returns a boolian wether a space is a secret path way or not
-    bool Get_Multi_Zone_status();
+    bool Get_Occupied_Status() const;//returns wether  a space is occupied or not
+    bool Get_Secret_Pathway_Status() const; //returns a boolian wether a space is a secret path way or not
+    bool Get_Multi_Zone_status() const;
     void Get_Zone_Color(std::array<ZONE_COLORS,3>& Passed_Array_To_Be_Filled);
+    int Get_Space_Number() const;
     void Set_Info(bool Is_Multi_zone, bool Is_Occupied, bool Has_Secret_Path, int Space_Number, const std::array<ZONE_COLORS, 3>& Colors);
 
     private:
