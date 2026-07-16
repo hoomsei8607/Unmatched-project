f22a33314c5c50c3dd7e9a94e0907091bd17ee18
piccy <farzadpiczol22@gmail.com>
2026-07-16 08:22:34 +0330

    Added: baptism of blood manager implementation

--- src/scheme_card_manager_class.cpp
+++ src/scheme_card_manager_class.cpp
@@ -93,7 +93,7 @@ void Scheme_Manager::Mistform_Screen(Controller& control)
         return vbox({
             vbox({
                 text(" "),
-                text("CHOOSEN CARD: MISTFORM") | bold | underlined | size(WIDTH, EQUAL, 90) | color(Color::NavajoWhite3),
+                text("CHOOSEN CARD: MISTFORM") | hcenter | bold | underlined | size(WIDTH, EQUAL, 90) | color(Color::NavajoWhite3),
                 text(" "),
                 text("MOVE DRACULA TO ONE OF THESE AVAILABLE SPACES WITHOUT LOSING YOUR TURN") | bold,
                 text(" "),
@@ -107,5 +107,99 @@ void Scheme_Manager::Mistform_Screen(Controller& control)
 void Scheme_Manager::Baptism_Of_Blood_Screen(Controller& control)
 {
     auto screen = ScreenInteractive::Fullscreen();
-    
+    bool Is_Any_Sister_Dead = control.Is_Any_Of_Dracula_Sisters_Dead();
+    USER user_who_owns_dracula;
+    Graph* game_map_ptr = Graph::Get_Map_Graph_Pointer();
+
+    if(control.Return_User1_Hero_Name() == HERO_NAME::DRACULA)
+    {
+        user_who_owns_dracula = USER::USER1;
+    }
+    else
+    {
+        user_who_owns_dracula = USER::USER2;
+    }
+
+
+
+    if(Is_Any_Sister_Dead)
+    {
+        std::set<int> available_spaces_to_place_the_revived_sister_on = game_map_ptr->return_all_spaces_with_the_corresponding_zones(game_map_ptr->return_zone(control.Return_Hero_Space_Number(Fighters_Names::DRACULA)));
+        std::vector<std::string> options_for_toggle_box;
+        int selected_space = 0;
+
+        for(int space : available_spaces_to_place_the_revived_sister_on)
+        {
+            options_for_toggle_box.push_back(std::to_string(space));
+        }
+
+        auto toggle_box = Toggle(&options_for_toggle_box, &selected_space);
+        auto confirm_button = Button("CONFIRM", [&]{
+            int space_number_for_revived_sis = std::stoi(options_for_toggle_box[selected_space]);
+            control.Revive_The_Selected_Fighter(control.Return_Dead_Sister_Number(), space_number_for_revived_sis);
+            game_map_ptr->Set_User_Occupying_Space(user_who_owns_dracula, space_number_for_revived_sis);
+            game_map_ptr->Change_Space_Occiupied_Status(space_number_for_revived_sis);
+
+            Space_Row_And_Column_In_Array temp_struct_to_update_fighter_position_on_screen;
+            control.Convert_Space_Number_To_Row_And_Column_Index(control.Return_Hero_Space_Number(Fighters_Names::DRACULA), temp_struct_to_update_fighter_position_on_screen);
+            control.fighters_printing_info_array[static_cast<int>(Fighters_Names::DRACULA)].Row_Index = temp_struct_to_update_fighter_position_on_screen.row_index;
+            control.fighters_printing_info_array[static_cast<int>(Fighters_Names::DRACULA)].Column_Index = temp_struct_to_update_fighter_position_on_screen.column_index;
+            screen.ExitLoopClosure()();
+        });
+        auto container = Container::Vertical({
+            toggle_box,
+            confirm_button
+        });
+
+        screen.Loop(Renderer(container, [&]{
+            return vbox({
+                vbox({
+                    text(" "),
+                    text("CHOOSEN CARD: BAPTISM OF BLOOD") | hcenter | bold | underlined | size(WIDTH, EQUAL, 90) | color(Color::NavajoWhite3),
+                    text(" "),
+                    text("DRACULA: +2HP") | bold,
+                    text(" "),
+                    text("DEAD SISTER: REVIVED") | bold,
+                    text(" "),
+                    text("SLECET WHERE TO PLACE THE REVIVED SISTER") | bold,
+                    text(" "),
+                    container -> Render()
+                }) | center
+            }) | border;
+
+        }));
+
+
+        
+
+
+    }
+    else
+    {
+        
+        auto continue_button = Button("CONTINUE", [&]{
+            screen.ExitLoopClosure()();
+        });
+        screen.Loop(Renderer(continue_button, [&]{
+            return vbox({
+                vbox({
+                    text(" "),
+                    text("CHOOSEN CARD: BAPTISM OF BLOOD") | hcenter | bold | underlined | size(WIDTH, EQUAL, 90) | color(Color::NavajoWhite3),
+                    text(" "),
+                    text("DRACULA: +2HP") | bold,
+                    text(" "),
+                    text("NO SISTER REVIVED") | bold,
+                    text(" "),
+                    continue_button -> Render()
+                }) | center
+            }) | border;
+            
+        }));
+    }
+    control.change_fighter_health(Fighters_Names::DRACULA, 2);
+    current_screen = SCHEME_CARDS_SCREEN::EXIT_TO_MAIN_LOOP;
+    if(control.Manage_UserAction_Numbers_And_Return_True_TO_Change_Turn())
+    {
+        control.Change_User_Turn();
+    }
 }
